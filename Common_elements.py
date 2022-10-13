aab195ae4b2697de25addbeaafadb53b3e2b6720
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-10-13 21:46:06 +0530

    a,b=map(int,input().split()) arr1=list(map(int,input().split())) arr2=list(map(int,input().split())) p=[] c=0 for i in arr1:     if i in arr2 and i not in p:         p.append(i) print(*p)

---
+++ Common_elements.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+arr1=list(map(int,input().split()))
+arr2=list(map(int,input().split()))
+p=[]
+c=0
+for i in arr1:
+    if i in arr2 and i not in p:
+        p.append(i)
+print(*p)
