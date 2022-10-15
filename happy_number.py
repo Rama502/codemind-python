4b80d35c7e4314972b619f45bb751deb8ce9550b
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-10-15 23:44:31 +0530

    def happy(a):     su=0     while(1):         su=0         while a>0:             rem=a%10             su+=rem**2             a//=10         if su<10:             break         else:             a=su             continue     if su==1 or su==7:         return 1     else:         return 0 a=int(input()) res=happy(a) if res==1:     print("True") else:     print("False")

--- happy_number.py
+++ happy_number.py
@@ -1,15 +1,23 @@
-def isnumber(num):
-    rem=sum=0;
-    while(num>0):
-        rem=num%10;
-        sum=sum+(rem*rem)
-        num=num//10;
-    return sum
-num=int(input())
-result=num
-while(result!=1 and result!=4):
-    result=isnumber(result)
-if(result==1):
+def happy(a):
+    su=0
+    while(1):
+        su=0
+        while a>0:
+            rem=a%10
+            su+=rem**2
+            a//=10
+        if su<10:
+            break
+        else:
+            a=su
+            continue
+    if su==1 or su==7:
+        return 1
+    else:
+        return 0
+a=int(input())
+res=happy(a)
+if res==1:
     print("True")
-elif(result==4):
+else:
     print("False")
