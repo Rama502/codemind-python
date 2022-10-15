fd2fc22eedd234a06330a6dfeefe619f26b329ef
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-10-15 23:33:09 +0530

    a=int(input()) f=0 if a>0:     while a>0:         if a%2==0:             a//=2         elif a%3==0:             a//=3         elif a%5==0:             a//=5         else:             f=1             break     if f==1:         if a==1:             print("Ugly Number")         else:             print("Not Ugly Number")     else:         print("Ugly Number") else:     print("Not Ugly Number")

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,15 +1,22 @@
-n=int(input())
-x=0
-while n!=1:
-    if n%5==0:
-        n=n//5
-    elif n%3==0:
-        n=n//3
-    elif n%2==0:
-        n=n//2
+a=int(input())
+f=0
+if a>0:
+    while a>0:
+        if a%2==0:
+            a//=2
+        elif a%3==0:
+            a//=3
+        elif a%5==0:
+            a//=5
+        else:
+            f=1
+            break
+    if f==1:
+        if a==1:
+            print("Ugly Number")
+        else:
+            print("Not Ugly Number")
     else:
-        print("Not Ugly Number")
-        x=1
-        break
-if x==0:
-    print("Ugly Number")
+        print("Ugly Number")
+else:
+    print("Not Ugly Number")
