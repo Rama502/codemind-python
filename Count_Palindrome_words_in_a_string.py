e86cce313c07f1c1e1fbb9686da569db278ea965
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-10-16 16:07:27 +0530

    s=list(map(str,input().split())) c=0 for i in s:     m=i.lower()     n=m[::-1]     if m==n:         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,10 +1,8 @@
-n=input()
-n1=n.lower()
-s=n1.split()
+s=list(map(str,input().split()))
 c=0
 for i in s:
-    k=i
-    p=i[::-1]
-    if k==p:
-        c=c+1
+    m=i.lower()
+    n=m[::-1]
+    if m==n:
+        c+=1
 print(c)
